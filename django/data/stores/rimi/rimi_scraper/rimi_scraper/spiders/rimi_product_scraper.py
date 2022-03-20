9fee324e68522d45709b57fe289db9fd6d7bc543
JmDajer <randel12310@gmail.com>
2022-03-20 20:59:55 +0200

    Work on rimi_scrapers price progress

--- django/data/stores/rimi/rimi_scraper/rimi_scraper/spiders/rimi_product_scraper.py
+++ django/data/stores/rimi/rimi_scraper/rimi_scraper/spiders/rimi_product_scraper.py
@@ -1,12 +1,16 @@
 import scrapy
+from scrapy.spiders import Rule
+from scrapy.linkextractors import LinkExtractor
 
 
 class RimiScraper(scrapy.Spider):
     name = "rimi_product_scraper"
+    rules = [Rule(LinkExtractor(allow=r'epood/ee/tooted.*/c/SH-.*\?page='), allow_domains='rimi.ee')]
     start_urls = ["https://www.rimi.ee/epood/ee/tooted/piimatooted-munad-juust/c/SH-11"]
 
     def parse(self, response):
-        for products in response.css("div.js-product-container card-horizontal-for-mobile"):
+        for products in response.css("div.card__details"):
             yield {
-                "info": products.css("div.data-gtm-eec-product").get(),
+                "name": products.css("p.card_name::text").get(),
+                "price": products.css("div.price-tag::text").get(),
             }
