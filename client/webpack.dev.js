da3bb929b771ed31fe0acc81016e83c29f212bd0
ae <90932525+ea-ae@users.noreply.github.com>
2022-03-20 03:12:23 +0200

    Create npm project
    Create npm project

---
+++ client/webpack.dev.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'eval',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'devdist'),
+        publicPath: '/',
+        clean: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer: {
+        port: 8080,
+        hot: true,
+        magicHtml: true,
+        static: { directory: path.join(__dirname, 'public') },
+        client: { progress: true },
+        compress: true,
+        port: 9001,
+        watchFiles: {paths: ['./src/**/*']},
+        historyApiFallback: {
+            index: '/',
+        },
+    },
+});
---
+++ client/webpack.prod.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: '[name].[contenthash].bundle.js',
+        publicPath: '/',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            chunks: 'all',
+            minSize: 35000, // 35kb
+        },
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ]
+});
