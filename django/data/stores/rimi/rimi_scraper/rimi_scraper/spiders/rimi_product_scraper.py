ac5bd96450470fc3e48c6b68021ae6a38e66d1fb
JmDajer <randel12310@gmail.com>
2022-03-20 23:45:20 +0200

    Modify rimi_product_scraper to find name, price, discount_price

--- django/data/stores/rimi/rimi_scraper/rimi_scraper/spiders/rimi_product_scraper.py
+++ django/data/stores/rimi/rimi_scraper/rimi_scraper/spiders/rimi_product_scraper.py
@@ -5,12 +5,23 @@ from scrapy.linkextractors import LinkExtractor
 
 class RimiScraper(scrapy.Spider):
     name = "rimi_product_scraper"
-    rules = [Rule(LinkExtractor(allow=r'epood/ee/tooted.*/c/SH-.*\?page='), allow_domains='rimi.ee')]
-    start_urls = ["https://www.rimi.ee/epood/ee/tooted/piimatooted-munad-juust/c/SH-11"]
+    rules = [Rule(LinkExtractor(allow=r'www.rimi.ee/epood/ee/tooted.*/c/SH-.*\?page='))]
+    start_urls = ["https://www.rimi.ee/epood/ee/tooted/piimatooted-munad-juust/c/SH-11?"
+                  "page=1&pageSize=80&query=%3Aprice-asc%3AallCategories%3ASH-11%3AassortmentStatus%3AinAssortment"]
 
     def parse(self, response):
         for products in response.css("div.card__details"):
-            yield {
-                "name": products.css("p.card_name::text").get(),
-                "price": products.css("div.price-tag::text").get(),
-            }
+            if f'{products.css("span::text").get()},{products.css("sup::text").get()}' == "None,None":
+                continue
+            elif not products.css("div.card__price-wrapper.-has-discount").get():
+                yield {
+                    "name": products.css("p.card__name::text").get(),
+                    "price": f'{products.css("span::text").get()},{products.css("sup::text").get()}',
+                    "discounted_price": None,
+                }
+            else:
+                yield {
+                    "name": products.css("p.card__name::text").get(),
+                    "price": products.css("div.old-price-tag.card__old-price span::text").get().replace('€', ''),
+                    "discounted_price": f'{products.css("span::text").get()},{products.css("sup::text").get()}',
+                }
