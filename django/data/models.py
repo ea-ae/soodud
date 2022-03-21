69a0d82d1bb42f167f34335df462201560059aeb
ae <90932525+ea-ae@users.noreply.github.com>
2022-03-21 02:26:22 +0200

    Add product tag model

--- django/data/models.py
+++ django/data/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
 
+class ProductTag(models.Model):
+    """Category or tag for a product."""
+    name = models.CharField(max_length=250)
+
+
 class Product(models.Model):
     """Platonic product that all individual store instances point to."""
     name = models.CharField(max_length=250)
-    description = models.TextField(null=True)
     image_url = models.TextField(null=True)
+    tags = models.ManyToManyField(ProductTag, blank=True, null=True)
 
 
 class Store(models.Model):
     """Store."""
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=250)
 
 
 class StoreProduct(models.Model):
@@ -21,14 +26,14 @@ class StoreProduct(models.Model):
     last_checked = models.DateTimeField(auto_now=True)
 
 
-class PriceGroup(models.Model):
+class Price(models.Model):
     """Current or historic price for a store product."""
     product = models.ForeignKey(StoreProduct, on_delete=models.CASCADE)
 
     current = models.BooleanField()  # is the price current, potential optimization over date sort?
-    start = models.DateTimeField(auto_now_add=False)  # some stores may specify a manual add-end date
-    end = models.DateTimeField(null=True)  # store-provided campaign end dates, null for historical prices
+    start = models.DateTimeField(auto_now_add=False)  # some stores may specify a manual add/end date
+    end = models.DateTimeField(blank=True, null=True)  # store-provided campaign end dates, null for historical prices
 
-    base_price = models.IntegerField(null=True)  # null if out of stock / unavailable
-    sale_price = models.IntegerField(null=True)  # null if no sale
+    base_price = models.IntegerField(blank=True, null=True)  # null if out of stock / unavailable
+    sale_price = models.IntegerField(blank=True, null=True)  # null if no sale
     members_only = models.BooleanField()
