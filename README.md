4722a03b3c01272353d05d665e2893a9791ee885
ae <90932525+ea-ae@users.noreply.github.com>
2022-04-17 17:21:21 +0300

    Update README and clean up codebase

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # soodud
 
-Webapp. :)
+Soodud is a webapp that scrapes data from online stores and uses C++ hierarchical cluster analysis to form comparable products between stores. The data is given by a Django REST API and then processed by a TailwindCSS & React frontend. CI/CD is implemented with Docker and static files are served by nginx.
--- django/data/services.py
+++ django/data/services.py
@@ -10,29 +10,7 @@ def launch():
     StoreRegistry.update_stores()
 
 
-def match(*, size=500_000, start_offset=0):
+def match():
     """Match products together."""
-    # stores = [StoreProduct.objects.filter(store=store.id).values('id', 'name')
-    #           for store in Store.objects.only('id').all()]
     from .stores import coop, selver, rimi
     StoreRegistry.match_stores()
-
-
-# def _match_product_set(limit: int, offset: int) -> int:
-#     """Match a set of products with a limit and offset."""
-#     matches = StoreProduct.objects.raw(
-#         f'''
-#         SELECT *
-#         FROM (
-#             SELECT sp.id AS id, sp.name AS name,
-#             FROM data_storeproduct sp
-#             WHERE
-#           ) sp
-#           INNER JOIN data_storeproduct sp2 ON (
-#             AND sp.id < sp2.id
-#             AND sp.store_id <> sp2.store_id
-#             AND (sp.has_barcode = FALSE OR sp2.has_barcode = FALSE)
-#           )
-#         OFFSET {offset} ROWS FETCH NEXT
-#         ''')
-#     text_analysis.prepare()
