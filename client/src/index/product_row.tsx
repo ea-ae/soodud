cf195fb51c4e7a9b01f3b39791feb9f14983a4ff
ae <90932525+ea-ae@users.noreply.github.com>
2022-04-30 22:40:10 +0300

    Add more stores to graph and assign them colors

--- client/src/index/product_row.tsx
+++ client/src/index/product_row.tsx
@@ -81,17 +81,17 @@ const ProductDetail = (props: {onClose: () => void, product: Product}) => { // m
 }
 
 const PriceHistoryChart = (props: {productName: string}) => {
-    let base = +new Date(2021, 1, 1);
+    let base = +new Date(2022, 1, 1);
     let oneDay = 24 * 3600 * 1000;
 
     const getData = () => {
         let date = [];
         let data = [];
         let last_data = 10;
-        for (let i = 1; i < 600; i++) {
+        for (let i = 1; i < 90; i++) {
             var now = new Date((base += oneDay));
             date.push([now.getFullYear(), now.getMonth() + 1, now.getDate()].join('/'));
-            let new_data = last_data + (Math.random() * 1.9 - 0.9);
+            let new_data = last_data + (Math.random() * 0.6 - 0.2);
             last_data = new_data;
             data.push(Number(new_data).toFixed(2));
         }
@@ -100,66 +100,82 @@ const PriceHistoryChart = (props: {productName: string}) => {
 
     let [date, data] = getData();
     let [date2, data2] = getData();
+    let [date3, data3] = getData();
+    let [date4, data4] = getData();
 
     const generateData = () => {
 
     }
 
     let options = {
-        grid: { top: 8, right: 8, bottom: 70, left: 36 },
-        toolbox: {
-            feature: {
-                // dataZoom: { yAxisIndex: 'none' },
-                restore: {},
-                saveAsImage: {},
-            },
-        },
-          xAxis: {
+        grid: { top: 8, right: 8, bottom: 70, left: 40 },
+        toolbox: { feature: { restore: {} } },
+        xAxis: {
             type: 'category',
             boundaryGap: false,
             data: date,
-          },
-          yAxis: {
-            name: '€',
+        },
+        yAxis: {
             type: 'value',
             scale: true,
             boundaryGap: [0, '100%'],
-          },
-          dataZoom: [
+            axisLabel: { formatter: '{value}€' }
+        },
+        dataZoom: [
             {
-              type: 'inside',
-              start: 0,
-              end: 100,
+                type: 'inside',
+                start: 0,
+                end: 100,
             },
             {
-              start: 0,
-              end: 100,
+                start: 0,
+                end: 100,
             },
-          ],
-          tooltip: {
-              trigger: 'axis',
-          },
-          series: [
+        ],
+        tooltip: {
+            trigger: 'axis',
+        },
+        series: [
             {
-                name: 'Coop',
                 data: data,
+                name: 'Coop',
                 type: 'line',
+                lineStyle: {color: '#0070cc'},
+                itemStyle: {color: '#0070cc'},
                 symbol: 'none',
                 sampling: 'lttb',
             },
             {
-                name: 'Selver',
                 data: data2,
+                name: 'Prisma',
                 type: 'line',
+                lineStyle: {color: '#088c44'},
+                itemStyle: {color: '#088c44'},
                 symbol: 'none',
                 sampling: 'lttb',
             },
-          ],
+            {
+                data: data3,
+                name: 'Rimi',
+                type: 'line',
+                lineStyle: {color: '#d72323'},
+                itemStyle: {color: '#d72323'},
+                symbol: 'none',
+                sampling: 'lttb',
+            },
+            {
+                data: data4,
+                name: 'Selver',
+                type: 'line',
+                lineStyle: {color: '#e8ce07'},
+                itemStyle: {color: '#e8ce07'},
+                symbol: 'none',
+                sampling: 'lttb',
+            },
+        ],
     };
 
-    return <ECharts
-            option={options}
-            style={{width: '100%'}} />
+    return <ECharts option={options} style={{width: '100%'}} />
 }
 
 const ProductName = (props: {name: string}) => {
